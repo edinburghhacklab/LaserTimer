893fed16606376d5bd70736017c90ee8b468fbf6
Martin Ling <martin-git@earth.li>
2013-06-19 03:43:34 +0100

    Move EEPROM operations to separate functions.

--- LaserTimer.ino
+++ LaserTimer.ino
@@ -6,9 +6,7 @@ unsigned long total;
 boolean laser_on;
 
 void setup() {
-  unsigned int i;
-  for (i = 0; i < sizeof(total); i++)
-    *(((byte *) &total) + i) = EEPROM.read(i);
+  readEEPROM(0, &total, sizeof(total));
   Serial.begin(115200);
   laser_on = digitalRead(2);
   attachInterrupt(0, inputChanged, CHANGE);
@@ -31,8 +29,18 @@ void inputChanged() {
 }
 
 void supplyFalling() {
-  unsigned int i;
-  for (i = 0; i < sizeof(total); i++)
-    EEPROM.write(i, *(((byte *) &total) + i));
+  writeEEPROM(0, &total, sizeof(total));
   while (1);
+}
+
+void readEEPROM(unsigned int src, void *dest, unsigned int size) {
+  unsigned int i;
+  for (i = 0; i < size; i++)
+    *(((byte *) dest) + i) = EEPROM.read(src + i);
+}
+
+void writeEEPROM(unsigned int dest, void *src, unsigned int size) {
+  unsigned int i;
+  for (i = 0; i < size; i++)
+    EEPROM.write(dest + i, *(((byte *) src) + i));
 }
